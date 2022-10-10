9a1193d0aa86c291d37bc44987c7d091e2cb8347
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2022-10-10 18:13:55 +0530

    Update to_count_bitsets.cpp

--- Bitset/to_count_bitsets.cpp
+++ Bitset/to_count_bitsets.cpp
@@ -3,6 +3,7 @@
 #define endl "\n"
 using namespace std;
 
+// Time complexity O(logn)
 int count_set_bits(int n)
 {
     int cnt = 0;
@@ -16,11 +17,24 @@ int count_set_bits(int n)
     }
     return cnt;
 }
+
+// Time Complexity O(no of set bits) i.e. in worst case same as above one
+int brian_kerninghan(int n)
+{
+    int cnt = 0;
+    while (n > 0)
+    {
+        n = n & (n - 1);
+        cnt++;
+    }
+    return cnt;
+}
 int main()
 {
     ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
     int n;
     cin >> n;
-    cout << count_set_bits(n);
+    // cout << brian_kerninghan(n);
+    cout << __builtin_popcount(n);
     return 0;
 }
