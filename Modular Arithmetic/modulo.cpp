2e057a6d3a403b3b0aed303db842561adff28c4b
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2022-11-29 11:42:37 +0530

    Update modulo.cpp

--- Modular Arithmetic/modulo.cpp
+++ Modular Arithmetic/modulo.cpp
@@ -1,3 +1,7 @@
 // Negative number modulo is given by :
 long long MOD = 1e9+7;
 cout << (n % MOD + MOD) % MOD << endl;
+
+// To get nearest multiple after a number
+// Assume y=10,k=6 then y1 will be 12 (nearest multiple of 6 after 10)
+y1 = (y - (y % k)) + k;
