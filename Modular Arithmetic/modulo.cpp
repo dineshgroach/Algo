6c9700ecd3e40ad168a14736e7969fb79510e43c
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2022-12-06 23:02:37 +0530

    update for 2/3 digits mulltiplication

--- Modular Arithmetic/modulo.cpp
+++ Modular Arithmetic/modulo.cpp
@@ -5,3 +5,7 @@ cout << (n % MOD + MOD) % MOD << endl;
 // To get nearest multiple after a number
 // Assume y=10,k=6 then y1 will be 12 (nearest multiple of 6 after 10)
 y1 = (y - (y % k)) + k;
+
+// Modulus for multiplicaton of 2/3 integers
+(a * b) % MOD = ((a % MOD) * (b % MOD)) % MOD
+(a * b * c) % MOD = ((a % MOD) * (b % MOD) * (c % MOD)) % MOD
