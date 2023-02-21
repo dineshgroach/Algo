ee4f5708f8276d00c6843dc8e54595ff895b1949
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2023-02-22 02:42:34 +0530

    Create find_nCr.cpp

---
+++ Maths/find_nCr.cpp
@@ -0,0 +1,14 @@
+// Using 1D DP approach
+int ncr(int n,int r)
+{
+    int res[r+1];
+    res[0]=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=min(i,r);j>0;j--)
+        {
+            res[j]=(res[j]+res[j-1])%1000000007;
+        }
+    }
+    return res[r];
+}
