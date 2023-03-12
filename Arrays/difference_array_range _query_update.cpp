d9838539cd9f29f920cf40f9b547076e90f30851
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2023-03-12 13:14:51 +0530

    Range Query Update / Difference Array Method

---
+++ Arrays/difference_array_range _query_update.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    // Initializing the main and difference array
+    vector<int> v = {2, 3, 6, 12, 18, 15};
+    int n = v.size();
+    vector<int> diff(n + 1, 0);
+
+    // populating the main array
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            diff[i] = v[i];
+        }
+        else
+        {
+            diff[i] = v[i] - v[i - 1];
+        }
+    }
+
+    // Start updating the range
+    int l = 2;
+    int r = 5;
+    int value = 4;
+    diff[l] = diff[l] + value;
+    diff[r + 1] = diff[r + 1] - value;
+
+    // Now update the prefix from diff array into the main array
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            v[i] = diff[i];
+        }
+        else
+        {
+            v[i] = diff[i] + diff[i - 1];
+            diff[i] = diff[i] + diff[i - 1];
+        }
+    }
+
+    // Same logic to update the value in diff array itself
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            diff[i] = diff[i];
+        }
+        else
+        {
+            diff[i] = diff[i] - diff[i - 1];
+        }
+    }
+
+    // To print the update value in the main array
+    for (auto i : v)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
