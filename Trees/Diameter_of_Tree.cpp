cde68cee41b13ca2dabdb16db40299769f0b2a8b
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2023-04-28 14:50:48 +0530

    To find diameter of Tree
    Assuming tree is given in the form of edges

---
+++ Trees/Diameter_of_Tree.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5 + 10;
+vector<int> g[N];
+vector<int> depth(N, 0);
+
+void dfs(int vertex, int par = -1)
+{
+    for (int child : g[vertex])
+    {
+        if (child == par)
+        {
+            continue;
+        }
+        depth[child] = depth[vertex] + 1;
+        dfs(child, vertex);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n - 1; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+    dfs(1);
+    // To calculate the farthest Node from Node 1
+    int mx_depth = -1;
+    int mx_depth_node;
+    for (int i = 1; i <= n; i++)
+    {
+        if (depth[i] > mx_depth)
+        {
+            mx_depth = depth[i];
+            mx_depth_node = i;
+        }
+    }
+    depth.assign(N, 0);
+    // To calculate the farthest Node from above found farthest Node
+    dfs(mx_depth_node);
+    for (int i = 1; i <= n; i++)
+    {
+        if (depth[i] > mx_depth)
+        {
+            mx_depth = depth[i];
+        }
+    }
+    cout << mx_depth << endl;
+    return 0;
+}
