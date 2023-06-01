4ce272da4e230864aa708b4501ce9fe9651b0904
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2023-06-01 20:07:33 +0530

    Update disjoint_set_union.cpp
    Added Better In Question Implentation

--- Graphs/disjoint_set_union.cpp
+++ Graphs/disjoint_set_union.cpp
@@ -63,3 +63,75 @@ int main()
     cout << connected_cnt << endl;
     return 0;
 }
+
+// Better in-question implementation of this
+int find(int v, vector<int> &par)
+{
+    if (par[v] == v)
+    {
+        return v;
+    }
+    return par[v] = find(par[v], par);
+}
+
+void make_union(int x, int y, vector<int> &par, vector<int> &siz)
+{
+    x = find(x, par);
+    y = find(y, par);
+    if (x != y)
+    {
+        if (siz[x] < siz[y])
+        {
+            swap(x, y);
+        }
+        par[y] = x;
+        siz[x] += siz[y];
+    }
+}
+
+void solve()
+{
+    int n, p, q;
+    cin >> n >> p >> q;
+    vector<int> p1(n + 1, 0), p2(n + 1, 0);
+    vector<int> s1(n + 1, 1), s2(n + 1, 1);
+    for (int i = 1; i <= n; i++)
+    {
+        p1[i] = i;
+        p2[i] = i;
+    }
+    for (int i = 0; i < p; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        make_union(x, y, p1, s1);
+    }
+    for (int i = 0; i < q; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        make_union(x, y, p2, s2);
+    }
+    vector<pair<int, int>> v;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = i + 1; j <= n; j++)
+        {
+            int x1 = find(i, p1);
+            int y1 = find(j, p1);
+            int x2 = find(i, p2);
+            int y2 = find(j, p2);
+            if (x1 != y1 and x2 != y2)
+            {
+                v.emplace_back(i, j);
+                make_union(i, j, p1, s1);
+                make_union(i, j, p2, s2);
+            }
+        }
+    }
+    cout << sz(v) << endl;
+    for (auto &it : v)
+    {
+        cout << it.first << " " << it.second << endl;
+    }
+}
