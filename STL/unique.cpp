619ce748426052617a65617d96f70277ebe36758
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2023-06-02 23:34:32 +0530

    Create unique.cpp

---
+++ STL/unique.cpp
@@ -0,0 +1,4 @@
+// it is used to remove duplicate set of values from the range and gives the iterator to the new starting of the container
+int m = unique(s.begin(), s.end()) - s.begin();
+
+// it convert "000111011" into "0101" and m is the size of the new string
