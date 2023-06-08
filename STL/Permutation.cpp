998722413c2cd5b96353ad9ff4ac62fcef1d0168
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2023-06-08 14:41:39 +0530

    Update Permutation.cpp

--- STL/Permutation.cpp
+++ STL/Permutation.cpp
@@ -1,1 +1,26 @@
+// STL used to create the unique permutation of a string or a vector, working is shown below
 
+    string s;
+    cin >> s;
+// If want to avoid duplicate permutations then sort it
+    sort(s.begin(), s.end());
+    vector<string> ans;
+    string chk = s;
+    ans.push_back(chk);
+    while (true)
+    {
+        next_permutation(s.begin(), s.end());
+        if (chk == s)
+        {
+            break;
+        }
+        else
+        {
+            ans.push_back(s);
+        }
+    }
+    cout << ans.size() << endl;
+    for (auto it : ans)
+    {
+        cout << it << endl;
+    }
