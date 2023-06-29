8a4e82add58ed18c4dd5ff6e4d9fb8e3d62c8c8d
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2023-06-29 15:24:39 +0530

    Create partial_sum.cpp

---
+++ STL/partial_sum.cpp
@@ -0,0 +1,9 @@
+// This stl is used to calculate the prefix sum of vector
+// It does not return anything works on the vector whose iterators are provided
+
+// if you want to store the prefix of array a into array b
+partial_sum(a, a + 5, b);
+// other wise if you want to do that in same thing can try out following syntax
+partial_sum(array.begin(),array.end(),array.begin());
+
+// as there is custome sort function in cast of sort stl you can add custom funciton here also
