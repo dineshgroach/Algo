02f09e7b48642cbad0c13d9709cff89b7dd744fa
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2023-07-02 11:11:33 +0530

    Update 0_1_KnapSack.cpp
    Added Space Optimized Solution

--- DP/KnapSack/0_1_KnapSack.cpp
+++ DP/KnapSack/0_1_KnapSack.cpp
@@ -30,3 +30,21 @@ int knapSack(int W, int wt[], int val[], int n)
     vector<vector<int>>dp(n+1,vector<int>(W+1,-1));
     return blackbox(n,W,wt,val,dp);
 }
+
+// Space Optimized soultion because we are only using the 1D DP
+int knapSack(int W, int wt[], int val[], int n) 
+    { 
+        int dp[W+1];
+        memset(dp,0,sizeof(dp));
+        for(int i = 1;i<= n;i++)
+        {
+            for(int w=W;w>=0;w--)
+            {
+                if(wt[i-1]<=w)
+                {
+                    dp[w]=max(dp[w],dp[w-wt[i-1]]+val[i-1]);
+                }
+            }
+        }
+        return dp[W];
+    }
