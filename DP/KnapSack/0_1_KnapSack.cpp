034aca6189b0bdd6a1e691687067547eec2804e2
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2023-07-02 10:48:17 +0530

    Create 0_1_KnapSack.cpp

---
+++ DP/KnapSack/0_1_KnapSack.cpp
@@ -0,0 +1,32 @@
+// 0 1 KnapSack GFG Problem
+int blackbox(int n,int w,int wt[],int val[],vector<vector<int>>&dp)
+{
+    if(n==0||w==0)
+    {
+        return 0;
+    }
+    // To check if the value is not being memoized
+    if(dp[n][w]!=-1)
+    {
+        return dp[n][w];
+    }
+
+    // If can include but choice is there to not include
+    if(wt[n-1]<=w)
+    {
+        dp[n][w]=max(val[n-1]+blackbox(n-1,w-wt[n-1],wt,val,dp),blackbox(n-1,w,wt,val,dp));
+        return dp[n][w];
+    }
+
+    // If we can include that element
+    if(wt[n-1]>w)
+    {
+        dp[n][w]=blackbox(n-1,w,wt,val,dp);
+    }
+}
+
+int knapSack(int W, int wt[], int val[], int n) 
+{ 
+    vector<vector<int>>dp(n+1,vector<int>(W+1,-1));
+    return blackbox(n,W,wt,val,dp);
+}
