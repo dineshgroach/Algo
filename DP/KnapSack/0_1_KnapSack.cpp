3c0139c18ff992c9e5e6bdcb1c53ebafc3c15172
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2023-07-02 13:00:42 +0530

    Update 0_1_KnapSack.cpp
    Added Iterative/Bottom-Up DP code

--- DP/KnapSack/0_1_KnapSack.cpp
+++ DP/KnapSack/0_1_KnapSack.cpp
@@ -1,4 +1,6 @@
 // 0 1 KnapSack GFG Problem
+
+// Below the code using recursion and memoization in top-down DP
 int blackbox(int n,int w,int wt[],int val[],vector<vector<int>>&dp)
 {
     if(n==0||w==0)
@@ -29,6 +31,31 @@ int knapSack(int W, int wt[], int val[], int n)
 { 
     vector<vector<int>>dp(n+1,vector<int>(W+1,-1));
     return blackbox(n,W,wt,val,dp);
+}
+
+// Below the iterative/tabulation or bottom up DP
+int knapSack(int W, int wt[], int val[], int n) 
+{ 
+    vector<vector<int>>dp(n+1,vector<int>(W+1));
+    for (int i = 0; i <=n; i++)
+    {
+        for (int j = 0; j <=W; j++)
+        {
+            if(i==0||j==0)
+            {
+                dp[i][j]=0;
+            }
+            else if(wt[i-1]<=j)
+            {
+                dp[i][j]=max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+            }
+            else
+            {
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+    }
+    return dp[n][W];
 }
 
 // Space Optimized soultion because we are only using the 1D DP
