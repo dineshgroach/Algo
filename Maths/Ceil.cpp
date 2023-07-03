f91b861efd7b54445b65e6a5a060307f7641d05c
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2023-07-03 23:41:26 +0530

    Create Ceil.cpp

---
+++ Maths/Ceil.cpp
@@ -0,0 +1,7 @@
+// custom function to get ceil of two numbers given in integer format works on the below principal
+// ceil(a/b) can be written as (a+b-1)/b in normal way
+
+int getceil(int64_t a,int64_t b)
+{
+    return (a+b-1)/b;
+}
