e5a4b2e9715dce0dd36b8bad855afe099982747f
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2023-07-23 13:18:58 +0530

    Update bit_manipulation.cpp

--- Bitset/bit_manipulation.cpp
+++ Bitset/bit_manipulation.cpp
@@ -36,3 +36,9 @@ int remove_setbit(int n)
 {
   return (n&(n-1));
 }
+
+// To convert a binary string into it's decimal form
+stoll(s,0,2);
+
+// To convert a number into a string of it binary form
+string s = bitset<32>(X).to_string();
