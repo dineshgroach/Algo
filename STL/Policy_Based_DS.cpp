93f498b581cd6a6db57cf1538dce1b02c32ba8ea
Dinesh Sharma <76250915+dineshgroach@users.noreply.github.com>
2024-03-03 13:02:18 +0530

    Update Policy_Based_DS.cpp
    added code about multiset

--- STL/Policy_Based_DS.cpp
+++ STL/Policy_Based_DS.cpp
@@ -14,4 +14,16 @@
 
 // if want to store the duplicate as well then try to use pbds with pairs Try pbds with pairs, insert{a[i],i}.
 // find number of values less than equal to some x in pbds with pairs. s.order_of_key({x, n+1})
-// where n=number of elements in array
+// where n=number of elements in array , another way to do same is to use ordered multiset
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template <class T>
+using ordered_multiset = __gnu_pbds::tree<T, __gnu_pbds::null_type, less_equal<T>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+
+// example decleration rest features are same
+    ordered_multiset<int>s;
